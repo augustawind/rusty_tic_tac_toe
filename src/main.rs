e75186de00e6006cc9ded8eb8cc1e2a3c91c2cec
Dustin Rohde <dustin.rohde@gmail.com>
2017-02-24 18:22:15 -0500

    Draw board, sketch data structures.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+type Board = [[u32; 3]; 3];
+
+enum Player {
+    X,
+    O,
+}
+
+enum GameState {
+    TurnX,
+    TurnO,
+    GameOver,
+}
+
+struct Game {
+    board: Board,
+    player_x: Player,
+    player_o: Player,
+    state: GameState,
+}
+
+fn draw_board(board: &Board) {
+    for row in board {
+        for code in row {
+            match *code {
+                0 => print!("."),
+                1 => print!("X"),
+                2 => print!("O"),
+                _ => panic!("Invalid value for board piece: {}", code),
+            }
+        }
+        println!("");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut board = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+    ];
+    draw_board(&board);
 }
