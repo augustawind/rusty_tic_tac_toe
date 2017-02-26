18e531508532a7309421d9fe3459ce4aa8145a44
Dustin Rohde <dustin.rohde@gmail.com>
2017-02-26 15:34:23 -0500

    Make Game functions methods of impl Game.

--- src/main.rs
+++ src/main.rs
@@ -33,145 +33,188 @@ struct Game {
     move_count: u32,
 }
 
-/// Draw the game board.
-fn draw_board(board: &Board) {
-    println!("");
-    for row in board {
-        for state in row {
-            print!("{}  ", state);
+impl Game {
+
+    fn new(size: usize, first_move: State) -> Game {
+        let board = vec![vec![State::Blank; size]; size];
+        Game {
+            board: board,
+            size: size,
+            next_move: first_move,
+            move_count: 0,
         }
+    }
+
+    /// Draw the game board.
+    fn draw_board(&self) {
+        println!("");
+        for row in &self.board {
+            for state in row {
+                print!("{}  ", state);
+            }
+            print!("\n\n");
+        }
+        println!("");
+    }
+
+    /// Execute a turn of the game, prompting for input and placing an X or O.
+    /// Returns the coordinates of the move.
+    fn turn(&mut self) -> (usize, usize) {
+        // Announce turn.
+        let round = ((self.move_count + 1) as f32 / 2.0).ceil() as u32;
         print!("\n\n");
-    }
-    println!("");
-}
+        println!("      Round {} - {}'s    ", round, self.next_move);
+        println!("  ----------------------");
 
-/// Execute a turn of the game, prompting for input and placing an X or O.
-/// Returns the coordinates of the move.
-fn turn(game: &mut Game) -> (usize, usize) {
-    // Announce turn.
-    let round = ((game.move_count + 1) as f32 / 2.0).ceil() as u32;
-    print!("\n\n");
-    println!("      Round {} - {}'s    ", round, game.next_move);
-    println!("  ----------------------");
+        loop {
+            // Prompt for move.
+            self.draw_board();
+            println!("Where will you move? [x,y]");
 
-    loop {
-        // Draw board.
-        draw_board(&game.board);
-        // Prompt for move.
-        println!("Where will you move? [x,y]");
+            let input: String = read!("{}\n");
+            let coords: Vec<&str> = input.split(',').collect();
 
-        let input: String = read!("{}\n");
-        let coords: Vec<&str> = input.split(',').collect();
-
-        // Ensure input is properly formatted.
-        if coords.len() != 2 {
-            println!("\nPlease enter two digits, separated by a comma.");
-            continue;
-        }
-
-        // Parse coords as usize.
-        let x: usize = match coords[0].parse() {
-            Ok(n) => n,
-            Err(_) => {
+            // Ensure input is properly formatted.
+            if coords.len() != 2 {
                 println!("\nPlease enter two digits, separated by a comma.");
                 continue;
             }
-        };
-        let y: usize = match coords[1].parse() {
-            Ok(n) => n,
-            Err(_) => {
-                println!("\nPlease enter two digits, separated by a comma.");
+
+            // Parse coords as usize.
+            let x: usize = match coords[0].parse() {
+                Ok(n) => n,
+                Err(_) => {
+                    println!("\nPlease enter two digits, separated by a comma.");
+                    continue;
+                }
+            };
+            let y: usize = match coords[1].parse() {
+                Ok(n) => n,
+                Err(_) => {
+                    println!("\nPlease enter two digits, separated by a comma.");
+                    continue;
+                }
+            };
+
+            // Ensure coords are in bounds.
+            if x >= self.size || y >= self.size {
+                println!("\nCoordinates out of bounds. Options are 0, 1, 2.");
                 continue;
             }
-        };
 
-        // Ensure coords are in bounds.
-        if x >= game.size || y >= game.size {
-            println!("\nCoordinates out of bounds. Options are 0, 1, 2.");
-            continue;
-        }
+            // Ensure space is empty for move.
+            if self.board[y][x] != State::Blank {
+                println!("\nSomeone has already moved there!");
+                continue;
+            }
 
-        // Ensure space is empty for move.
-        if game.board[y][x] != State::Blank {
-            println!("\nSomeone has already moved there!");
-            continue;
-        }
+            // Place board piece and iterate turn.
+            self.move_count += 1;
 
-        // Place board piece and iterate turn.
-        game.move_count += 1;
+            match self.next_move {
+                State::X => {
+                    self.board[y][x] = State::X;
+                    self.next_move = State::O;
+                },
+                State::O => {
+                    self.board[y][x] = State::O;
+                    self.next_move = State::X
+                },
+                State::Blank => panic!("This should never happen."),
+            };
 
-        match game.next_move {
-            State::X => {
-                game.board[y][x] = State::X;
-                game.next_move = State::O;
-            },
-            State::O => {
-                game.board[y][x] = State::O;
-                game.next_move = State::X
-            },
-            State::Blank => panic!("This should never happen."),
-        };
-
-        return (x, y);
-    }
-}
-
-/// Return whether the game is over, plus the winner, or Blank if its a draw.
-fn is_game_over(game: &Game, state: State, x: usize, y: usize) -> (bool, State) {
-    let n = game.size;
-
-    // Check columns
-    for i in 0..n {
-        if game.board[i][x] != state {
-            break;
-        }
-        if i == n - 1 {
-            return (true, state);
+            return (x, y);
         }
     }
-    // Check rows
-    for i in 0..n {
-        if game.board[y][i] != state {
-            break;
-        }
-        if i == n - 1{
-            return (true, state);
-        }
-    }
-    // Check diagonal
-    if x == y {
+
+    /// Return whether the game is over, plus the winner, or Blank if its a draw.
+    fn is_game_over(&self, state: State, x: usize, y: usize) -> (bool, State) {
+        let n = self.size;
+
+        // Check columns
         for i in 0..n {
-            if game.board[i][i] != state {
+            if self.board[i][x] != state {
                 break;
             }
             if i == n - 1 {
                 return (true, state);
             }
         }
-    }
-    // Check anti-diagonal
-    if x + y == n - 1 {
+        // Check rows
         for i in 0..n {
-            if game.board[i][n - 1 - i] != state {
+            if self.board[y][i] != state {
                 break;
             }
-            if i == n - 1 {
+            if i == n - 1{
                 return (true, state);
             }
         }
+        // Check diagonal
+        if x == y {
+            for i in 0..n {
+                if self.board[i][i] != state {
+                    break;
+                }
+                if i == n - 1 {
+                    return (true, state);
+                }
+            }
+        }
+        // Check anti-diagonal
+        if x + y == n - 1 {
+            for i in 0..n {
+                if self.board[i][n - 1 - i] != state {
+                    break;
+                }
+                if i == n - 1 {
+                    return (true, state);
+                }
+            }
+        }
+
+        // Check draw
+        if self.move_count == (n * n) as u32 {
+            return (true, State::Blank);
+        }
+
+        // Game continues
+        (false, State::Blank)
     }
 
-    // Check draw
-    if game.move_count == (n * n) as u32 {
-        return (true, State::Blank);
+    /// Draw the final board and announce the winner or draw.
+    fn end(&self, winner: State) {
+        // Display final board
+        println!("");
+        self.draw_board();
+        println!("");
+        println!("Game over!");
+
+        // Announce winner or draw
+        match winner {
+            State::Blank => println!("It's a draw!"),
+            _            => println!("{}'s win!", winner),
+        }
     }
 
-    // Game continues
-    (false, State::Blank)
+    fn run(&mut self) {
+        let mut game_over = false;
+        let mut winner = State::Blank;
+
+        // Loop until game over
+        while !game_over {
+            let last_state = self.next_move;
+            let (x, y) = self.turn();
+            let result = self.is_game_over(last_state, x, y);
+            game_over = result.0;
+            winner = result.1;
+        }
+
+        self.end(winner);
+    }
 }
 
 /// Display the game header.
-fn header() {
+fn show_header() {
     println!("");
     println!("===========================");
     println!("=                         =");
@@ -227,51 +270,13 @@ fn prompt_first_move() -> State {
     }
 }
 
-/// Draw the final board and announce the winner or draw.
-fn conclusion(board: &Board, winner: State) {
-    // Display final board
-    println!("");
-    draw_board(board);
-    println!("");
-    println!("Game over!");
-
-    // Announce winner or draw
-    match winner {
-        State::Blank => println!("It's a draw!"),
-        _            => println!("{}'s win!", winner),
-    }
-}
 
 /// Play a game from start to finish.
-fn play() {
-    header();
-
+fn main() {
+    show_header();
     let size = prompt_game_size();
     let first_move = prompt_first_move();
 
-    let board = vec![vec![State::Blank; size]; size];
-
-    let mut game = Game {
-        board: board,
-        size: size,
-        next_move: first_move,
-        move_count: 0,
-    };
-
-    let (mut game_over, mut winner) = (false, State::Blank);
-
-    // Loop until game over
-    while !game_over {
-        let last_state = game.next_move;
-        let (x, y) = turn(&mut game);
-        let result = is_game_over(&game, last_state, x, y);
-        game_over = result.0;
-        winner = result.1;
-    }
-
-    conclusion(&game.board, winner);
-}
-
-fn main() {
-    play();
+    let mut game = Game::new(size, first_move);
+    game.run();
 }
